31a44d021955a0ad541f9aa22b656f6fcdc9d8eb
Manel <quemonmanel@gmail.com>
2022-10-03 20:14:02 +0200

    Ejercicio01-Realizado

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 
 
-let num1 = 10; 
+let num1 = 45; 
 
 let num2 = 45; 
 
-if (num1 >= num2) {
+if (num1 > num2) {
 
 console.log (num1); 
 
+}else if   (num2 > num1) { 
+
+
+    console.log (num2);
+
+}else if (num1 === num2) {
+
+    console.log ((num1), (num2));
 }
+ 
