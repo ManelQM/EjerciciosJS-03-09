383813b955be80f10300d2229cecc5756c9c0256
Manel <quemonmanel@gmail.com>
2022-10-03 21:46:12 +0200

    Ejercicio04-Realizado

---
+++ Ejercicio04/js/main.js
@@ -0,0 +1,9 @@
+
+const pi = 3.14; 
+
+let radio = prompt("Por favor introduzca el radio del círculo para calcular el área"); 
+
+let area = pi*(radio**2); 
+
+console.log("El área es igual a" , area); 
+
