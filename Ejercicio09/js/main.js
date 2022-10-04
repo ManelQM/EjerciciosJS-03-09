f48937ef67501115263e5a241324d3d0eb06604f
Manel <quemonmanel@gmail.com>
2022-10-04 19:02:00 +0200

    Ejercicio09-JSvanilla

---
+++ Ejercicio09/js/main.js
@@ -0,0 +1,16 @@
+
+
+for(let i = 1; i <=100; i++) {
+
+    if (i % 2 === 0) {
+
+        console.log ('Número divisible por 2', i);
+
+
+    }else if (i % 3 === 0) {
+
+        console.log ('Número divisible por 3', i);
+    }
+    
+
+}
