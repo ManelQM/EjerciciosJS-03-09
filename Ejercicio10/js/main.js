58b2a22985e0862efa769a373998fc63f6d5e57b
Manel <quemonmanel@gmail.com>
2022-10-04 21:09:40 +0200

    Ejercicio10-realizado

---
+++ Ejercicio10/js/main.js
@@ -0,0 +1,16 @@
+
+
+let ventas = parseInt(prompt('Por favor, introduzca el número de ventas')); 
+
+for(let i = 0; i < ventas; i++) {
+
+    
+let tantasventas = parseInt(prompt('Por favor, introduzca tantas ventas por teclado como ...')); 
+   
+console.log (ventas + tantasventas); 
+}
+
+ 
+
+
+
